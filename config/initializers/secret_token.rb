cba9bc461ffcfbd0aaa72a66a69bf3413fcbfbdc
Bruno Bacarini <bruno.bacarini@locaweb.com.br>
2013-10-08 20:24:19 -0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NgdecorSystem::Application.config.secret_key_base = '748265bb0a2c1101062598330e88229d27c8d24dc2715391060e643fd8a8aa0ba419a219ff15dd6975a2166a02b3549d95f9b7ecb5735bd26373db35d62d7be5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+NgdecorSystem::Application.config.session_store :cookie_store, key: '_ngdecor-system_session'
