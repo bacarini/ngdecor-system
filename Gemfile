c5e0a70785e570c9fa74e960fc65a5d2e5cbfc85
Bruno Bacarini <bruno.bacarini@locaweb.com.br>
2013-10-23 11:16:13 -0200

    add gem roo

--- Gemfile
+++ Gemfile
@@ -14,7 +14,7 @@ gem 'sass-rails', '~> 4.0.0'
 gem 'coffee-rails', '~> 4.0.0'
 gem 'uglifier', '>= 1.0.3'
 gem 'simple_form'
-
+gem 'roo'
 
 group :doc do
   # bundle exec rake doc:rails generates the API under doc/api.
--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -69,6 +69,6 @@ class CustomersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def customer_params
-      params.require(:customer).permit(:name, :phone, :mobile, :address)
+      params.require(:customer).permit(:name, :phone, :mobile, :address, :architect)
     end
 end
