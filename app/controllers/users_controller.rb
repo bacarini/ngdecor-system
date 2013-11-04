62b2e08756c1dbf2cfb4858e082ea280c9161a3b
Bruno Bacarini <bruno.bacarini@locaweb.com.br>
2013-11-04 15:47:17 -0200

    add CRUD on users (devise)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,34 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:destroy]
+
   def index
     @users = User.all
   end
 
-  def create
-  # @user = User.new(email: params[:email],
-  #                  password: params[:password],
-  #                  password_confirmation: params[:password])
-  end
-
   def new
     @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to users_url, notice: t('users.create.success') 
+    else
+      render action: 'new' 
+    end
+  end
+
+  def destroy
+    @user.destroy
+    redirect_to users_url 
+  end
+
+  private
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ NgdecorSystem::Application.routes.draw do
 
   resources :sellers
 
-  devise_for :users
-
+  devise_for :users, path_prefix: 'my_user'
   resources :users
+
   resources :customers
   resources :imports, only: [:new,:create]
   resources :products 
