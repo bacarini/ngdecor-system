8aa623ab6cf847cee79af81330164833b0e941c0
Bruno Bacarini <bruno.bacarini@locaweb.com.br>
2013-11-06 20:31:19 -0200

    new text on import page

--- app/assets/javascripts/import.js
+++ app/assets/javascripts/import.js
@@ -1,5 +1,5 @@
 App.imports = {};
 
 App.imports["new"] = function() {
-    $(":file").filestyle({classButton: "btn btn btn-success", buttonText: I18n.t("select_file")});
+    $(":file").filestyle({classButton: "btn", buttonText: I18n.t("select_file")});
 };
