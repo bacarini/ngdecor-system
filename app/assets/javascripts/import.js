5b3da703a8013241a715f70f8acdb43b84a40d07
Bruno Bacarini <bruno.bacarini@locaweb.com.br>
2013-11-07 19:16:47 -0200

    new validations on views

--- app/assets/javascripts/import.js
+++ app/assets/javascripts/import.js
@@ -3,15 +3,29 @@ App.imports = {};
 App.imports["new"] = function() {
     $(":file").filestyle({classButton: "btn", buttonText: I18n.t("select_file")});
 
-    $(".import").validate({
+    $("#new_import").validate({
+      errorElement: "span",
+      errorClass:'help-inline beforeElement',
+      errorPlacement: function (error, element) {
+        error.insertBefore(element);
+      },
+      highlight: function(label) {    
+        $(label).closest('.control-group').addClass('error');
+      },
+      success: function(label) {
+        $(label).closest('.control-group').removeClass('error');
+      },
       rules: {
         'import[file]': {
           required: true,
-          extension: "xls|csv|xlsx"
+      extension: "xls|csv|xlsx"
         }
       },
       messages: {
-        'import[file]': "Extesão de arquivo inválida!"               
+        'import[file]': {
+          extension: "Extesão de arquivo inválida!",
+          required : I18n.t("errors.messages.blank")
+        }
       }
     });
 };
--- app/assets/javascripts/supplier.js
+++ app/assets/javascripts/supplier.js
@@ -1,6 +1,41 @@
 App.suppliers = {};
 
-App.suppliers.after = function() {
+App.suppliers.before = function() {
   $("#supplier_cnpj").mask(I18n.t("mask.cnpj"));
   $("#supplier_phone").mask(I18n.t("mask.phone"));
 };
+App.suppliers["new"] = function(){
+  $('#new_supplier').validate({
+    errorElement: "span",
+    errorClass:'help-inline',
+    highlight: function(label) {    
+      $(label).closest('.control-group').addClass('error');
+    },
+    errorPlacement: function (error, element) { 
+      if (element.parent().hasClass("input-append")){
+        error.insertAfter(element.parent());
+      }else{
+        error.insertAfter(element);     
+      } 
+    },                   
+    onkeyup: false,      
+    onblur: false,  
+    success: function(label) {
+      $(label).closest('.control-group').removeClass('error');
+    },
+    rules:{
+      'supplier[cnpj]': {
+        cnpj     : 'both'
+      }
+    },
+    messages:{
+      'supplier[cnpj]': {
+        cnpj     : I18n.t("errors.messages.cnpj_invalid"),
+        required : I18n.t("errors.messages.blank")
+      },
+      'supplier[name]': {
+        required : I18n.t("errors.messages.blank")
+      }
+    } 
+  });
+}
