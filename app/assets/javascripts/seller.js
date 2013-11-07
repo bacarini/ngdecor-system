fd6e1facbb194d0c9a640ab305e5b90680225af1
Bruno Bacarini <bruno.bacarini@locaweb.com.br>
2013-11-07 19:29:11 -0200

    add validation on seller views

--- app/assets/javascripts/seller.js
+++ app/assets/javascripts/seller.js
@@ -4,3 +4,51 @@ App.sellers.after = function() {
   $("#seller_cnpj").mask(I18n.t("mask.cnpj"));
   $("#seller_phone").mask(I18n.t("mask.phone"));
 };
+App.sellers["new"] = function(){
+  validateSellers();
+};
+App.sellers["edit"] = function(){
+  validateSellers();
+};
+
+function validateSellers(){
+  $('.seller_validate').validate({
+    errorElement: "span",
+    errorClass:'help-inline',
+    highlight: function(label) {    
+      $(label).closest('.control-group').addClass('error');
+    },
+    errorPlacement: function (error, element) { 
+      if (element.parent().hasClass("input-append")){
+        error.insertAfter(element.parent());
+      }else{
+        error.insertAfter(element);     
+      } 
+    },                   
+    onkeyup: false,      
+    onblur: false,  
+    success: function(label) {
+      $(label).closest('.control-group').removeClass('error');
+    },
+    rules:{
+      'seller[cnpj]': {
+        cnpj     : 'both'
+      },
+      'seller[email]':{
+        email: true
+      }
+    },
+    messages:{
+      'seller[cnpj]': {
+        cnpj     : I18n.t("errors.messages.cnpj_invalid"),
+        required : I18n.t("errors.messages.blank")
+      },
+      'seller[name]': {
+        required : I18n.t("errors.messages.blank")
+      },
+      'seller[email]':{
+        email: "Email inválido"
+      }
+    } 
+  });
+}
